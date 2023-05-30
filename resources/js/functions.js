a670621f70e00c913860d305e74c506d31aaa26d
FranPetri <franbehrendt@hotmail.com.ar>
2023-05-30 08:45:24 -0300

    mod funciones js

--- resources/js/functions.js
+++ resources/js/functions.js
@@ -3,9 +3,5 @@ import { pedidos } from "./api.js";
 const array_pedidos = Array()
 pedidos.then(pedido => array_pedidos.push(...pedido));
 console.log(array_pedidos)
-
-
-
-
 // const tabla = document.getElementById("tabla");
 
