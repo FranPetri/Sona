580df7c7110324335e6130423dbd1905b8899148
FranPetri <franbehrendt@hotmail.com.ar>
2023-05-31 11:44:39 -0300

    cambio de estado

--- resources/js/api.js
+++ resources/js/api.js
@@ -1,9 +1,11 @@
 async function getPedidos() {
-    const response = await fetch("../resources/js/pedidos.json");
+    const response = await fetch("../resources/js/pedidos.json",
+    { "mode": "no-cors",}
+    );
     const jsonData = await response.json();
     return jsonData
 }
-
+//Guarda la matriz con los datos de la Api y los exporta
 const pedidos = getPedidos();
 export{
     pedidos,
--- resources/js/functions.js
+++ resources/js/functions.js
@@ -1,18 +1,49 @@
 import { pedidos } from "./api.js"; //Importamos el json extraido de la API
 
-
-function addTime(hora) {
-    
-}
-
+let tabla = document.getElementById("tabla");
+let contador_pedidos = 0;
+ 
 // Agregar pedido a cada fila respecto a su hora
 function addTimePedido(solicitud,hora) {
-    let hora_maxima = 16; //Contador hasta hora maxima
-    for (let i = 7; i <= hora_maxima; i++) {
-        if (hora[0] == i) { //Si encuentra la hora correspondiente
-            let horario = document.getElementById("td"+i); //Tomamos la fila correspondiente a la hora
-            horario.innerHTML += "<td>"+solicitud+"</td>"; //Agregamos el pedido
+    const exploded_hora = hora[1].split(":"); //split de la hora para separar la hora de los minutos
+    for (let i = 0; i <= exploded_hora[0]; i++) {
+        let horario_tabla = document.getElementById("tr"+i);
+        if (horario_tabla == null) {
+            if (i == 0) {
+                tabla.innerHTML += '<tr id="tr'+i+'"><td>12 AM</td></tr>';
+            }else{
+                if (i >= 12) {
+                    tabla.innerHTML += '<tr id="tr'+i+'"><td>'+i+' PM</td></tr>';
+                    
+                }else{
+                    tabla.innerHTML += '<tr id="tr'+i+'"><td>'+i+' AM</td></tr>';
+                }
+            }
         }
+        if (hora[2] == "AM" && exploded_hora[0]==12) {
+            let pedidos_tabla = document.getElementById("tr0");
+            pedidos_tabla.innerHTML += '<td class="'+solicitud+'">'+solicitud+'</td>';//Tomamos la fila correspondiente a la hora 
+
+        }else if (exploded_hora[0] == i) { //Si encuentra la hora correspondiente
+            let pedidos_tabla = document.getElementById("tr"+i);
+            pedidos_tabla.innerHTML += '<td class="'+solicitud+'">'+solicitud+'</td>';//Tomamos la fila correspondiente a la hora
+        }
+    }
+    contador_pedidos++;
+}
+console.log(tabla)
+function colorPedido(estado,solicitud) {
+    if (estado == "Finalizado") {
+        return '<td class="back_green">'+solicitud+'</td>'
+    }
+    if (estado == "Pendientes") {
+        return '<td class="back_yellow">'+solicitud+'</td>'
+    }
+    if (estado == "Rechazado") {
+        return '<td class="back_red">'+solicitud+'</td>'
+    }
+    if (estado == "A realizar") {
+        return '<td class="back_white">'+solicitud+'</td>'
     }
 }
 
@@ -20,14 +51,20 @@ function addTimePedido(solicitud,hora) {
 pedidos.then(pedido => {
     pedido.forEach(detalle => {
         // Informacion y desestructuracion del objeto (Pedido)
-        const {Hora} = detalle; //Hora del pedido
+        const {FechaRealizacion} = detalle; //Hora del pedido
         const {NroSolicitud} = detalle; //Numero de solicitud del pedido
+        const {Estado} = detalle; //Estado del pedido
 
+        //Dar color correspondiente a su estado
+        colorPedido(Estado,NroSolicitud)
+        
         // Exlpoded de la hora para agregar a su respectiva fila
-        const exploded_time = Hora.split(" ") //Split de la fecha para tomar la hora
-        const exploded_hora = exploded_time[1].split(":"); //split de la hora para separar la hora de los minutos
-        addTimePedido(NroSolicitud,exploded_hora)
+        const exploded_time = FechaRealizacion.split(" ") //Split de la fecha para tomar la hora
+        addTimePedido(NroSolicitud,exploded_time)
 
 
+
+        // Contador de Pedidos
+        td_pedidos_total.innerText = "Pedidos en curso: Total " + contador_pedidos;
     });
 });
