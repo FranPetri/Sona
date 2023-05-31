fb4d947662ad02d3431490bd42c6f7b73344a52f
FranPetri <franbehrendt@hotmail.com.ar>
2023-05-31 09:41:05 -0300

    addPedido

--- resources/js/functions.js
+++ resources/js/functions.js
@@ -1,28 +1,33 @@
-import { pedidos } from "./api.js";
+import { pedidos } from "./api.js"; //Importamos el json extraido de la API
 
-let id_horarios = ["td7","td8","td9","td10","td11","td12","td13","td14","td15","td16"]
-const set_horarios = new Set()
-const horarios = []
-let contador = 7
 
-id_horarios.forEach(element => {
-    set_horarios.add(element)
-});
-set_horarios.forEach(element => {
-    horarios[element] = 7
-});
-
-function addTime(solicitud,hora) {
+function addTime(hora) {
+    
 }
 
+// Agregar pedido a cada fila respecto a su hora
+function addTimePedido(solicitud,hora) {
+    let hora_maxima = 16; //Contador hasta hora maxima
+    for (let i = 7; i <= hora_maxima; i++) {
+        if (hora[0] == i) { //Si encuentra la hora correspondiente
+            let horario = document.getElementById("td"+i); //Tomamos la fila correspondiente a la hora
+            horario.innerHTML += "<td>"+solicitud+"</td>"; //Agregamos el pedido
+        }
+    }
+}
+
+// Promesa pedido x pedido
 pedidos.then(pedido => {
     pedido.forEach(detalle => {
-        const {Hora} = detalle;
-        const {NroSolicitud} = detalle;
+        // Informacion y desestructuracion del objeto (Pedido)
+        const {Hora} = detalle; //Hora del pedido
+        const {NroSolicitud} = detalle; //Numero de solicitud del pedido
 
-        const exploded_time = Hora.split(" ")
-        const exploded_hora = exploded_time[1].split(":");
- 
-        addTime(NroSolicitud,exploded_hora)
+        // Exlpoded de la hora para agregar a su respectiva fila
+        const exploded_time = Hora.split(" ") //Split de la fecha para tomar la hora
+        const exploded_hora = exploded_time[1].split(":"); //split de la hora para separar la hora de los minutos
+        addTimePedido(NroSolicitud,exploded_hora)
+
+
     });
 });
